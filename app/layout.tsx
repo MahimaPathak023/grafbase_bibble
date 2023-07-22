8927e21c623e773aa395a548fb0af58937ffcc8a
MahimaPathak023 <pathakmahima57@gmail.com>
2023-07-22 22:58:11 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+
+export const metadata = {
+  title: 'Bibble',
+  description: 'Showcase your best developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+    
+      <body>
+        <Navbar/>
+        <main>
+        {children}
+        </main>
+       < Footer/>
+        </body>
+    </html>
+  )
+}
